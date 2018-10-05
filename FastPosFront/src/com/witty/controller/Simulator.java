4743424ba77801287558ceccc64c5b512330b3b6
Diego Andrés <andruco3@gmail.com>
2018-10-05 08:07:13 -0500

    Se actualiza interfaz del simulador

--- FastPosFront/src/com/witty/controller/Simulator.java
+++ FastPosFront/src/com/witty/controller/Simulator.java
@@ -2,15 +2,34 @@ package com.witty.controller;
 
 import java.io.IOException;
 
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.witty.entity.CamposModel;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
 
 public class Simulator {
+	
+	@FXML
+	private JFXTabPane tabPaneSimulator;
+	
+	@FXML
+	private JFXTreeTableView<CamposModel> tableViewServer;
+	
+	@FXML
+	private JFXTreeTableView<CamposModel> tableViewClient;
+	
+	@FXML
+	private JFXTreeTableView<CamposModel> tableViewCases;
 	
 	
 	@FXML
 	public void initialize() throws IOException {
 		
 		System.out.println("Soy un evolucionado");
+		
+		
 	}
 
 
